03e43f0f7602f1d747b4d6662e03bd27a3a16d54
Prabhukumar <a.b.prabhu268@gmail.com>
2023-11-17 20:46:39 +0530

    changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,8 @@
-import React from "react";  
+import React from "react";   
+import video  from "../assets/video.mp4"
 
-const src =
-  "https://room8studio.com/wp-content/uploads/2022/06/r8s.mp4";
+// const src =
+//   "https://room8studio.com/wp-content/uploads/2022/06/r8s.mp4";
 
 function Home (){
    
@@ -14,13 +15,13 @@ function Home (){
             <div className="quote1">
                 <p>WE'RE SMART</p>
             </div> 
-            <div className="quote2  ">
+            <div className="quote2">
                 <p className="font-effect-outline">WITH ART</p>
-            </div> </span>
+            </div> </span> 
             </div> 
             <div className="quote3">
                 <p className="para">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>  </div><span> <video autoPlay loop muted className="video"  width="1000px" >
-      <source src={src} type="video/mp4" />
+      <source src={video} type="video/mp4" />
       Sorry, your browser doesn't support embedded videos.
     </video></span>
            
